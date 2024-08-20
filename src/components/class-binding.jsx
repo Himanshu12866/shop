ad1fcfb292bf1c45ddf4850bcc9e60a37a19b8d6
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 15:00:32 +0530

    "Added state and event handlers to ClassBinding component to change text color on mouse over"

--- src/components/class-binding.jsx
+++ src/components/class-binding.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react"
 
 
 
-export default function ClassBinding(){
-    return(
-        <div>
-          <div className="row">
-            <div className="col-4" style={{backgroundColor:"red"}}>RED</div>
-            <div className="col-4" style={{backgroundColor:"blue"}}>BLUE</div>
-            <div className="col-4" style={{backgroundColor:"green"}}>GREEN</div>
-          </div>
+export default function ClassBinding() {
+
+    const [rang, setColor] = useState({
+        color: "black"
+    })
+    function ChangeColor(e) {
+        setColor({ color: e.target.id })
+    }
+    function OriginalColor() {
+        setColor({color:"black"})
+    }
+    return (
+        <div className="container-fluid">
+            <div className="row m-3">
+                <div onMouseOver={ChangeColor} onMouseOut={OriginalColor} id="red" className="col-4" style={{ height: "50px", color: "white", textAlign: "center", padding: "10px", backgroundColor: "red" }}>RED</div>
+                <div onMouseOver={ChangeColor} onMouseOut={OriginalColor} id="blue" className="col-4" style={{ height: "50px", color: "white", textAlign: "center", padding: "10px", backgroundColor: "blue" }}>BLUE</div>
+                <div onMouseOver={ChangeColor} onMouseOut={OriginalColor} id="green" className="col-4" style={{ height: "50px", color: "white", textAlign: "center", padding: "10px", backgroundColor: "green" }}>GREEN</div>
+            </div>
+
+            <div>
+                <h1 style={rang}>Change text color on mouse ever the color</h1>
+            </div>
         </div>
     )
 }
