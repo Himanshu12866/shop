faae63c0288c400b1f912d2c4485519443862aa8
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 15:56:44 +0530

    "Updated data.json and iphone-hover.jsx: changed image sources in data.json, added new state and function to iphone-hover.jsx, and modified JSX elements"

--- src/components/iphone-hover.jsx
+++ src/components/iphone-hover.jsx
@@ -1,29 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export default function HoverImg(){
-    const [image ,setImage] = useState({
+export default function HoverImg() {
+    const [image, setImage] = useState([{
         src: ""
-    })
-    function LoadData(){
+    }])
+    const [newImg, changeImg] = useState("m1.png")
+    function LoadData() {
         axios.get("data.json")
-        .then(response => {
-            setImage(response.data)
-        })
+            .then(response => {
+                setImage(response.data)
+            })
     }
     useEffect(() => {
         LoadData();
+
     })
+    function ChangeImage(e) {
+        changeImg(e.target.src)
+    }
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-4">
+                <div className="col-1">
                     {
-                        image.map((img_src,index)=>
+                        image.map(imgsrc =>
 
-                        <img alt="imgname" key={index} src={img_src}></img>
+                            <div className="m-3">
+                                <img alt="imgname" onMouseOver={ChangeImage} src={imgsrc.img_src} style={{ width: "100px" }}></img>
+                            </div>
                         )
                     }
+                </div>
+                <div className="col-4">
+                    <img style={{ width: "100%" }} src={newImg} alt="nameimage"></img>
                 </div>
             </div>
         </div>
