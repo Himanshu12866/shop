7b9141fa095c332de911f3e49ff50faa1df50bf7
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 16:32:48 +0530

    "Updated data.json and iphone-hover.jsx files with formatting changes, added new state variables and functions, and modified JSX elements."

--- src/components/iphone-hover.jsx
+++ src/components/iphone-hover.jsx
@@ -1,11 +1,35 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "../styles/iphone.css"
 
 export default function HoverImg() {
     const [image, setImage] = useState([{
         src: ""
+
     }])
+    const [data, setData] = useState({
+        title: "",
+        price: 0,
+        rating: {
+            rate: 0,
+            ratings: 0,
+            reviews: 0,
+        },
+        offers: []
+    })
     const [newImg, changeImg] = useState("m1.png")
+    const [styles, setStyle] = useState({
+        border: "",
+    })
+
+    function LoadData1() {
+        axios.get("data2.json").then(response => {
+            setData(response.data)
+        })
+    }
+    useEffect(() => {
+        LoadData1()
+    })
     function LoadData() {
         axios.get("data.json")
             .then(response => {
@@ -15,7 +39,12 @@ export default function HoverImg() {
     useEffect(() => {
         LoadData();
 
-    })
+    }, [])
+    useEffect(() => {
+        setStyle(
+
+        )
+    }, [])
     function ChangeImage(e) {
         changeImg(e.target.src)
     }
@@ -26,7 +55,7 @@ export default function HoverImg() {
                     {
                         image.map(imgsrc =>
 
-                            <div className="m-3">
+                            <div className="m-3" style={styles}>
                                 <img alt="imgname" onMouseOver={ChangeImage} src={imgsrc.img_src} style={{ width: "100px" }}></img>
                             </div>
                         )
@@ -34,6 +63,13 @@ export default function HoverImg() {
                 </div>
                 <div className="col-4">
                     <img style={{ width: "100%" }} src={newImg} alt="nameimage"></img>
+                </div>
+                <div className="col-7">
+                    <h3>{
+                        data.title
+                    }</h3>
+                   <h4> &#8377; {data.price.toLocaleString()}</h4>
+
                 </div>
             </div>
         </div>
