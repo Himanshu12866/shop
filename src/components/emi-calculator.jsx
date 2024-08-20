b5ee9b03faf0247f33418c2118decad14a848802
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 19:05:04 +0530

    "Updated EMI calculator component with additional fields and modal, and added Bootstrap CSS and JS imports to index.js"

--- src/components/emi-calculator.jsx
+++ src/components/emi-calculator.jsx
@@ -4,48 +4,89 @@ import "../styles/emi.css";
 
 
 export default function EMIcalculator() {
-    const [amount , setAmount] = useState(0)
-    function ChnageAmount(e){
-setAmount(e.target.value)
+    const [amount, setAmount] = useState(100000)
+    function ChnageAmount(e) {
+        setAmount(e.target.value)
+    }
+    const [year, setYear] = useState(1)
+    function ChangeYear(e) {
+        setYear(e.target.value)
+    }
+    const [rate, setRate] = useState(10.5)
+    function ChangeRate(e) {
+        setRate(e.target.value)
     }
     return (
         <div className="d-flex justify-content-center w-100 align-items-center" style={{ height: "100vh" }}>
 
-            <div className="row">
+            <div className="row p-3">
                 <h1>Personal EMI Calculator</h1>
                 <div className="col-4">
                     <label className="form-label fw-bold">Amount You Need :</label>
                     <input className="form-control" type="number" onChange={ChnageAmount} value={amount}></input>
                     <div className="d-flex justify-content-between my-2">
-                        <div> Min:&#8377; 1,00,00 </div>
+                        <div> Min:&#8377; 10,00,00 </div>
                         <div> Max:&#8377; 10,00,000</div>
                     </div>
                     <div className="my-2">
-                        <input type="range" onChange={ChnageAmount} className="form-range" value={amount} min={10000} max={1000000}/>
+                        <input type="range" onChange={ChnageAmount} className="form-range" value={amount} min={100000} max={1000000} />
                     </div>
 
                 </div>
                 <div className="col-4">
                     <label className="form-label fw-bold">For Years :</label>
-                    <input className="form-control" type="number" ></input>
+                    <input className="form-control" type="number" onChange={ChangeYear} value={year} ></input>
                     <div className="d-flex justify-content-between my-2">
                         <div> Min: 1 Yrs </div>
                         <div> Max: 5 Yrs</div>
                     </div>
                     <div className="my-2">
-                        <input type="range" className="form-range" min={1} max={5} />
+                        <input type="range" onChange={ChangeYear} className="form-range" value={year} min={1} max={5} />
                     </div>
                 </div>
                 <div className="col-4">
                     <label className="form-label fw-bold">Interest Rate :</label>
-                    <input className="form-control"></input>
+                    <input className="form-control" value={rate} onChange={ChangeRate}></input>
                     <div className="d-flex justify-content-between my-2">
                         <div>Min : 10.5 %</div>
                         <div>Max : 21 %</div>
                     </div>
                     <div className="my-2">
-                        <input type="range" className="form-range"  min={10.5} max={21} step={0.5}/>
+                        <input type="range" className="form-range" value={rate} onChange={ChangeRate} min={10.5} max={21} step={0.1} />
                     </div>
+                </div>
+                <button data-bs-target="#modalId" data-bs-toggle="modal" className="btn btn-dark">Calculate</button>
+                <div className="modal fade" id="modalId">
+                <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h1>Your EMI Result</h1>
+                <button className="btn btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div className="modal-body">
+                    <table className="table table-hover">
+                    <tbody>
+                        <tr className="fw-bold">
+                            <td>Amount : </td>
+                            <td>{amount.toLocaleString('en-in', {style:"currency", currency:"INR"})}</td>
+                        </tr>
+                        <tr className="fw-bold">
+                            <td> Years <span className="bi bi-clock"></span> : </td>
+                            <td>{year} Yr</td>
+                        </tr>
+                        <tr className="fw-bold">
+                            <td>Amount : </td>
+                            <td>{amount.toLocaleString('en-in', {style:"currency", currency:"INR"})}</td>
+                        </tr>
+                    </tbody>
+
+                    </table>
+                </div>
+
+                </div>
+
+                </div>
+
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 // import ClassBinding from './components/class-binding';
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
-import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 // import HoverImg from './components/iphone-hover';
 // import StyleBind from './components/style-binding';
 import EMIcalculator from './components/emi-calculator';
