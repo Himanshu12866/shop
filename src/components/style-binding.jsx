037c908226422e12544f3963d5d19ee3651fc94f
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 17:39:15 +0530

    "Removed img hover animation and transition, updated CSS keyframes, and modified JSX to use state for dynamic animation styles on img element"

--- src/components/style-binding.jsx
+++ src/components/style-binding.jsx
@@ -3,15 +3,24 @@ import './style-binding.css'
 
 export default function StyleBind(){
 
-    const [style, setStyle] = useState('img');
+    const [styles, setStyle] = useState({
+        animation: "rotate 6s linear infinite"
+    });
 
     function handleHeight(){
-        setStyle("image")
+        setStyle({
+            animation : "onHover .6s linear infinite"
+        })
+    }
+    function leaveMouse(){
+        setStyle({
+            animation : "rotate 6s linear infinite"
+        })
     }
 
     return(
         <div >
-          <img src="logo512.png" alt="ndn"></img>
+          <img src="logo512.png" onMouseLeave={leaveMouse} onMouseOver={handleHeight} style={styles} alt="ndn"></img>
         </div>
     )
 }
