2bc7ca1b8cd18fda9463a6af49d75c84fbcbf24d
Himanshu12866 <manshu010m@gmail.com>
2024-08-20 14:43:08 +0530

    "Added bootstrap and bootstrap-icons dependencies, updated package-lock.json and package.json, and modified src/index.js to import bootstrap CSS and render ClassBinding component instead of App"

---
+++ src/components/class-binding.jsx
@@ -0,0 +1,14 @@
+
+
+
+export default function ClassBinding(){
+    return(
+        <div>
+          <div className="row">
+            <div className="col-4" style={{backgroundColor:"red"}}>RED</div>
+            <div className="col-4" style={{backgroundColor:"blue"}}>BLUE</div>
+            <div className="col-4" style={{backgroundColor:"green"}}>GREEN</div>
+          </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ClassBinding from './components/class-binding';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ClassBinding />
   </React.StrictMode>
 );
 
