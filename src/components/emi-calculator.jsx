3756763e7f2a94419e6d565a071988c23d4469a0
Himanshu12866 <manshu010m@gmail.com>
2024-08-21 15:12:08 +0530

    "Added EMI calculation functionality and updated modal content to display EMI result"

--- src/components/emi-calculator.jsx
+++ src/components/emi-calculator.jsx
@@ -16,6 +16,18 @@ export default function EMIcalculator() {
     function ChangeRate(e) {
         setRate(e.target.value)
     }
+
+    const [emi, setEmi] = useState(0)
+
+    function emiPM() {
+        let p = amount;
+        let r = rate / 12 / 100;
+        let n = year * 12;
+        let EMI = p * r * (Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+        setEmi(Math.floor(EMI).toLocaleString("en-in", {"style":"currency" , "currency":"INR"}))
+
+
+    }
     return (
         <div className="d-flex justify-content-center w-100 align-items-center" style={{ height: "100vh" }}>
 
@@ -55,37 +67,43 @@ export default function EMIcalculator() {
                         <input type="range" className="form-range" value={rate} onChange={ChangeRate} min={10.5} max={21} step={0.1} />
                     </div>
                 </div>
-                <button data-bs-target="#modalId" data-bs-toggle="modal" className="btn btn-dark">Calculate</button>
+                <button onClick={emiPM} data-bs-target="#modalId" data-bs-toggle="modal" className="btn btn-dark">Calculate</button>
                 <div className="modal fade" id="modalId">
-                <div className="modal-dialog modal-dialog-centered">
-                <div className="modal-content">
-                <div className="modal-header">
-                    <h1>Your EMI Result</h1>
-                <button className="btn btn-close" data-bs-dismiss="modal"></button>
-                </div>
-                <div className="modal-body">
-                    <table className="table table-hover">
-                    <tbody>
-                        <tr className="fw-bold">
-                            <td>Amount : </td>
-                            <td>{amount.toLocaleString('en-in', {style:"currency", currency:"INR"})}</td>
-                        </tr>
-                        <tr className="fw-bold">
-                            <td> Years <span className="bi bi-clock"></span> : </td>
-                            <td>{year} Yr</td>
-                        </tr>
-                        <tr className="fw-bold">
-                            <td>Amount : </td>
-                            <td>{amount.toLocaleString('en-in', {style:"currency", currency:"INR"})}</td>
-                        </tr>
-                    </tbody>
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1>Your EMI Result</h1>
+                                <button className="btn btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div className="modal-body">
+                                <table className="table table-hover">
+                                    <tbody>
+                                        <tr className="fw-bold">
+                                            <td>Amount <sapn className="bi bi-currency-rupee"></sapn>: </td>
+                                            <td>{amount.toLocaleString('en-in', { style: "currency", currency: "INR" })}</td>
+                                        </tr>
+                                        <tr className="fw-bold">
+                                            <td> Years <span className="bi bi-clock"></span> : </td>
+                                            <td>{year} Yr</td>
+                                        </tr>
+                                        <tr className="fw-bold">
+                                            <td>Interest Rate <span className="bi bi-percent"></span>: </td>
+                                            <td>
+                                                {rate} %
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>EMI/M  <span className="bi bi-stack"></span>:</td>
+                                            <td className="fw-bold">{emi}</td>
+                                        </tr>
+                                    </tbody>
 
-                    </table>
-                </div>
+                                </table>
+                            </div>
 
-                </div>
+                        </div>
 
-                </div>
+                    </div>
 
                 </div>
             </div>
