e4170318cc1bfad012f6aa14835d5f1035e5a837
Himanshu12866 <manshu010m@gmail.com>
2024-08-21 17:44:51 +0530

    "Updated Overlay component with new styles and functionality"

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -1,9 +1,25 @@
 
+import { useState } from "react"
 import "../styles/overlay.css"
-export default function OverLay(){
-return (
-    <div className="box">
+export default function OverLay() {
+    const [style1, setStyle] = useState("overlay")
 
-    </div>
-)
+    function ChangeStyle() {
+        setStyle("overlay1")
+    }
+    function CngStl(){
+        setStyle("overlay")
+    }
+    return (
+        <div className="box">
+            <div className="child">
+                <button className="bi bi-list" onClick={ChangeStyle}></button>
+                <div className={style1}>
+            <button className="btn btn-close" onClick={CngStl}></button>
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
